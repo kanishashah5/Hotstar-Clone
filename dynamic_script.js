585f16a2ef9588547665ed0a681897c246d426c6
kanishashah5 <kanishah0505@gmail.com>
2024-07-20 20:14:55 +0530

    commit

---
+++ dynamic_script.js
@@ -0,0 +1,445 @@
+let list = [
+    {
+        id: 32,
+        name: "IND vs AUS WC FINALS 2023",
+        des: "Intense showdown, cricket glory: India vs Australia",
+        image: "images/poster 32.jpg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: true,
+    },
+    {
+        id: 4,
+        name: "Avengers : Endgame ",
+        des: "Epic heroes unite, vanquishing threats, altering destinies",
+        image: "images/poster 4.png",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 43,
+        name: "The Vaccine War",
+        des: "Global struggle: The Vaccine War's impactful narrative",
+        image: "images/poster 43.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 3,
+        name: "The Falcon and the Winter Soldier",
+        des: "Heroes unite, navigating challenges, forging powerful legacy",
+        image: "images/poster 3.png",
+        recommended: true,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 13,
+        name: "Loki",
+        des: "God of Mischief's cosmic escapades defy expectations",
+        image: "images/poster 13.jpeg",
+        recommended: true,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 15,
+        name: "MS Dhoni : The Untold Story",
+        des: "Dhoni's journey: captaincy, resilience, triumph, untold saga.",
+        image: "images/poster 15.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 17,
+        name: "Radha Krishna",
+        des: "Divine love story transcends time, eternal devotion",
+        image: "images/poster 17.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: true,
+    },
+    {
+        id: 18,
+        name: "Koffee With Karan",
+        des: "Celebrities spill secrets, candid conversations, entertaining revelations",
+        image: "images/poster 18.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 23,
+        name: "RRR",
+        des: "Epic saga, fierce battles, heroic camaraderie unfolds",
+        image: "images/poster 23.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 30,
+        name: "Tu hai Mera Sunday",
+        des: "Heartwarming tale, friendship, Mumbai's vibrant spirit",
+        image: "images/poster 30.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 2,
+        name: "Mulan(Hindi)",
+        des: "Warrior's journey, honor, bravery in Disney's adaptation",
+        image: "images/poster 2.png",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 28,
+        name: "Live Telecast",
+        des: "Horror reality, chilling consequences on-screen drama",
+        image: "images/poster 28.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 22,
+        name: "Devo ke dev : Mahadev",
+        des: "Mythical saga, divine journey, Lord Shiva's valor",
+        image: "images/poster 22.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 14,
+        name: "Spiderman : Far from Home",
+        des: "Heroic web-slinger faces global challenges, adventure",
+        image: "images/poster 14.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 27,
+        name: "Secret Invasion",
+        des: "Marvel's shapeshifting thriller, infiltration, cosmic stakes",
+        image: "images/poster 27.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 26,
+        name: "Avengers Endgame(Hindi)",
+        des: "Epic heroes unite, vanquishing threats, altering destinies",
+        image: "images/poster 26.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 24,
+        name: "Venom",
+        des: "Antihero's symbiotic saga, power struggle, formidable foes",
+        image: "images/poster 24.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 21,
+        name: "Thor : Love and Thunder",
+        des: "Thunderous love, cosmic adventures, electrifying spectacle",
+        image: "images/poster 21.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: true,
+    },
+    {
+        id: 12,
+        name: "Dark Phoenix",
+        des: "Mutant turmoil, destructive power, X-Men's ultimate challenge",
+        image: "images/poster 12.png",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 6,
+        name: "Avengers(Hindi)",
+        des: "Earth's mightiest heroes unite, defend against threats",
+        image: "images/poster 6.png",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: true,
+    },
+    {
+        id: 20,
+        name: "Black Panther : Wakanda Forever",
+        des: "Legacy continues, royal heritage, Marvel's tribute",
+        image: "images/poster 20.jpeg",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: true,
+    },
+    {
+        id: 9,
+        name: "Raya And the Last Dragon",
+        des: "Animated epic, mystical journey, courage triumphs",
+        image: "images/poster 9.png",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 10,
+        name: "Luca",
+        des: "Pixar's seaside adventure, friendship, sea monster secrets",
+        image: "images/poster 10.png",
+        recommended: false,
+        superheroes: true,
+        specials: false,
+        friends: false,
+    },
+    {
+        id: 16,
+        name: "Rudra : The Edge Of Darkness",
+        des: "Crime-solving thriller, darkness unraveled, riveting drama",
+        image: "images/poster 16.jpeg",
+        recommended: false,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 33,
+        name: "Sultan Of Delhi",
+        des: "Political intrigue, power struggles, royal legacy unfolds",
+        image: "images/poster 33.jpeg",
+        recommended: false,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 34,
+        name: "Mansion 24",
+        des: "Mysterious mansion, secrets, supernatural encounters, suspenseful narrative",
+        image: "images/poster 34.jpeg",
+        recommended: false,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 35,
+        name: "Aarya(Season 3)",
+        des: "Aarya's saga continues, family dynamics, gripping suspense",
+        image: "images/poster 35.jpeg",
+        recommended: false,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 36,
+        name: "MY3 : A Robotic Love Story",
+        des: "Futuristic love, AI complexities, captivating robotic tale",
+        image: "images/poster 36.jpeg",
+        recommended: false,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 37,
+        name: "City Of Dreams (Season 3)",
+        des: "Political drama, power clashes, Mumbai's intricate dynamics",
+        image: "images/poster 37.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 38,
+        name: "9 Hours",
+        des: "Thriller unfolds in real-time, suspense, ticking clock",
+        image: "images/poster 38.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 39,
+        name: "Kerala Crime Files",
+        des: "Intriguing crime narratives, investigations, Kerala's criminal world",
+        image: "images/poster 39.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 40,
+        name: "Taaza Khabar",
+        des: "Fresh news perspectives, media drama, timely revelations",
+        image: "images/poster 40.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 42,
+        name: "Criminal Justice : Adhura Sach",
+        des: "Legal drama, unfinished truths, complex justice system",
+        image: "images/poster 42.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: false,
+    },
+    {
+        id: 41,
+        name: "The Night Manager(Part II)",
+        des: "Espionage sequel, intrigue deepens, captivating continuation",
+        image: "images/poster 41.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 31,
+        name: "Houseful 2",
+        des: "Hilarious chaos, mistaken identities, uproarious ensemble comedy",
+        image: "images/poster 31.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 25,
+        name: "Total Dhamal",
+        des: "Madcap adventure, treasure hunt, laugh-out-loud Bollywood comedy",
+        image: "images/poster 25.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 29,
+        name: "Chhichhore",
+        des: "Nostalgic college days, friendship, life's valuable lessons",
+        image: "images/poster 29.jpeg",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+    {
+        id: 7,
+        name: "Pirates of the Carribean : Salazar's Revenge",
+        des: "Captain Jack Sparrow's swashbuckling return, supernatural adversaries",
+        image: "images/poster 7.png",
+        recommended: true,
+        superheroes: false,
+        specials: true,
+        friends: true,
+    },
+];
+
+function createCard() {
+    var Recommended = document.getElementById('Recommended');
+    var Superheroes = document.getElementById('Superheroes');
+    var Specials = document.getElementById('Specials');
+    var WatchwithFriends = document.getElementById('WatchwithFriends');
+
+    var cardContainer = document.createElement('div');
+    cardContainer.setAttribute('class', 'card-container');
+
+    list.map(item => {
+        var card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        card.setAttribute('id', item.id);
+
+        var img = document.createElement('img');
+        img.src = item.image;
+        img.setAttribute('class', 'card-img');
+        img.setAttribute('alt', item.name);
+
+        var cardBody = document.createElement('div');
+        cardBody.setAttribute('class', 'card-body');
+
+        var contentBody = document.createElement('div');
+        contentBody.setAttribute('class', 'content-body');
+
+        var name = document.createElement('h2');
+        name.setAttribute('class', 'name');
+        name.innerText = item.name;
+
+        var des = document.createElement('h6');
+        des.setAttribute('class', 'des');
+        des.innerText = item.des;
+
+        contentBody.appendChild(name);
+        contentBody.appendChild(des);
+
+        var watchlist = document.createElement('button');
+        watchlist.setAttribute('class', 'watchlist-btn');
+        watchlist.innerText = 'add to watchlist';
+
+        cardBody.appendChild(contentBody);
+        cardBody.appendChild(watchlist);
+
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        
+        if(item.recommended){
+            Recommended.appendChild(card)
+        }
+        if (item.superheroes) {
+            Superheroes.appendChild(card)
+        }
+        if (item.specials) {
+            Specials.appendChild(card)
+        }
+        if (item.friends) {
+            WatchwithFriends.appendChild(card)
+        }
+    });
+}
+
+createCard();
---
+++ main.js
@@ -0,0 +1,4 @@
+function tick(id){
+    var x = id+'✓';
+    document.getElementById(id).innerHTML=x;
+}
